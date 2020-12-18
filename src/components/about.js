a4fed64109a46b8fc6d338dc6c00b3b79fdbc4fa
hindra pangadi ghozalli <hindrapangadi@gmail.com>
2020-12-18 13:12:17 +0700

    update CSS

--- src/components/about.js
+++ src/components/about.js
@@ -8,34 +8,34 @@ const About = () => {
     return (
         <div class="container">
         <h1 class="heading"><span>meet</span>Our Team</h1>
-        
-        <div class="profiles">
-          <div class="profile">
-            <img src={dipta} class="profile-img" />
-            
-            <h3 class="user-name">Adipta M.</h3>
-            <h5>Managing Partner</h5>
-            <p>Our business partner in this project</p>
+        <div class="profile-container">
+          <div class="profiles">
+            <div class="profile">
+              <img src={dipta} class="profile-img" />
+              
+              <h3 class="user-name">Adipta M.</h3>
+              <h5>Managing Partner</h5>
+              <p>Our business partner in this project</p>
+            </div>
+          
+    
+            <div class="profile">
+              <img src={hindra} class="profile-img" />
+              
+              <h3 class="user-name">Hindra Pangadi Ghozalli</h3>
+              <h5>Project Manager</h5>
+              <p>Our manager and director for the project</p>
+            </div>
+          
+      
+            <div class="profile">
+              <img src={zefanya} class="profile-img" />
+              
+              <h3 class="user-name">Zefanya</h3>
+              <h5>Creative Director</h5>
+              <p>Our creative director in this project</p>
+            </div>
           </div>
-        
-   
-          <div class="profile">
-            <img src={hindra} class="profile-img" />
-            
-            <h3 class="user-name">Hindra Pangadi Ghozalli</h3>
-            <h5>Project Manager</h5>
-            <p>Our manager and director for the project</p>
-          </div>
-        
-     
-          <div class="profile">
-            <img src={zefanya} class="profile-img" />
-            
-            <h3 class="user-name">Zefanya</h3>
-            <h5>Creative Director</h5>
-            <p>Our creative director in this project</p>
-          </div>
-        
         </div>
       </div>
     );
